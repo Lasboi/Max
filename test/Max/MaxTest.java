85e36e0e9395945998910e5ed08c2eae6b4c49c0
LasBoi <Azlj1689gk>
2019-10-08 16:45:18 +0200

    Initial commit

---
+++ test/Max/MaxTest.java
@@ -0,0 +1,25 @@
+package Max;
+
+import junit.framework.TestCase;
+
+public class MaxTest extends TestCase {
+
+    /*
+        Test Case
+        Input: a = 4, b = 6
+        Forventet output: 6
+     */
+
+    public void testMaxOf4And6Equals6() {
+        // Input
+        int a = 4;
+        int b = 6;
+
+        // Execution
+        int result = Calculator.max(a, b);
+
+        // Asserts
+        assertEquals(6, result);
+    }
+
+}
